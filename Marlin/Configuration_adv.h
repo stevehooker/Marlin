4ffdac58d451f3ca92498991b6a95832ad9efb19
Steve Hooker <thewitchphotographer@gmail.com>
2020-07-09 13:44:24 +0100

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -717,6 +717,7 @@
   // Define probe X and Y positions for Z1, Z2 [, Z3 [, Z4]]
   // If not defined, probe limits will be used.
   // Override with 'M422 S<index> X<pos> Y<pos>'
+  #define Z_STEPPER_ALIGN_XY { {  20, 110 }, { 200,  110 } }
 
   /**
    * Orientation for the automatically-calculated probe positions.
