541fc8b8443c649a46d6e9d84bd32d616cf8c637
Steve Hooker <thewitchphotographer@gmail.com>
2020-08-02 17:55:49 +0100

    increase ez

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -749,14 +749,14 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 1600, 410 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 640, 640, 400, 456.04 }
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 10, 25 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
